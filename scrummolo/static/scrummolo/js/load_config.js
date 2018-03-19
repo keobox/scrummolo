e900badbfefaf34820474a734bbe73e622f1a3de
Cesare Placanica <ceplacan@cisco.com>
2018-03-19 22:04:11 +0100

    Game module

---
+++ scrummolo/static/scrummolo/js/load_config.js
@@ -0,0 +1,10 @@
+
+// Scrummolo game starts from here
+
+// Start jquery
+$(function() {
+    // Get config data from REST API
+    $.getJSON($SCRIPT_ROOT + '/scrummolo/api/v1.0/configs/1', function(data) {
+        scrummolo(data.config);
+    });
+});
--- scrummolo/static/scrummolo/js/scrummolo.js
+++ scrummolo/static/scrummolo/js/scrummolo.js
@@ -1,6 +1,7 @@
 
-$(function() {
-    $.getJSON($SCRIPT_ROOT + '/scrummolo/api/v1.0/configs/1', null, function(data) {
-        console.log(data.config);
-    });
-});
+// Game module
+
+function scrummolo(config) {
+    // game code goes here
+    console.log(config);
+}
