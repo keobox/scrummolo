38a2f43c9765879d533719da8eabd96d01ce48a3
Cesare Placanica <ceplacan@cisco.com>
2018-03-23 17:46:58 +0100

    Start porting to js

--- scrummolo/static/scrummolo/js/scrummolo.js
+++ scrummolo/static/scrummolo/js/scrummolo.js
@@ -2,7 +2,53 @@
 // Game module
 
 function scrummolo(config) {
+
     // game code goes here
-    console.log("hello from the game");
-    console.log(config);
+
+    var game_engine_config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        scene : {
+            preload : preload,
+            create: create,
+            update: update
+        }
+    }
+
+    var game = new Phaser.Game(game_engine_config);
+
+    // Fisher-Yates
+    function shuffle(array) {
+        var m = array.length, t, i;
+        // While there remain elements to shuffle…
+        while (m) {
+          // Pick a remaining element…
+          i = Math.floor(Math.random() * m--);
+          // And swap it with the current element.
+          t = array[m];
+          array[m] = array[i];
+          array[i] = t;
+        }
+        return array;
+    }
+
+    function preload() {
+        console.log(config);
+        var team = shuffle(config.team);
+        var playerImagePaths = [];
+        team.forEach(function(player) {
+            playerImagePaths.push(config.resources + '/' + player.toLowerCase() + '.png');
+        });
+        // TODO continue
+        // var playerImages = [];
+    }
+
+    function create() {
+
+    }
+
+    function update() {
+
+    }
 }
