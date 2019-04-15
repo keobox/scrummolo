3051832473af68755ea3d431c9ef9b96408139ac
Cesare Placanica <ceplacan@cisco.com>
2019-04-15 14:19:11 +0200

    Display an image

--- scrummolo/static/scrummolo/js/load_config.js
+++ scrummolo/static/scrummolo/js/load_config.js
@@ -5,6 +5,7 @@
 $(function() {
     // Get config data from REST API
     $.getJSON($SCRIPT_ROOT + '/scrummolo/api/v1/configs/1', function(data) {
-        scrummolo(data.config);
+        app.setConfig(data.config);
+        var game = new Phaser.Game(game_engine_config);
     });
 });
--- scrummolo/static/scrummolo/js/scrummolo.js
+++ scrummolo/static/scrummolo/js/scrummolo.js
@@ -1,54 +1,54 @@
 
 // Game module
 
-function scrummolo(config) {
-
-    // Fisher-Yates
-    function shuffle(array) {
-        var m = array.length, t, i;
-        // While there remain elements to shuffle…
-        while (m) {
-          // Pick a remaining element…
-          i = Math.floor(Math.random() * m--);
-          // And swap it with the current element.
-          t = array[m];
-          array[m] = array[i];
-          array[i] = t;
-        }
-        return array;
+// Fisher-Yates
+function shuffle(array) {
+    var m = array.length, t, i;
+    // While there remain elements to shuffle…
+    while (m) {
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+        // And swap it with the current element.
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
     }
+    return array;
+}
 
-    // Game Code
+// Game Code
 
-    var game_engine_config = {
-        type: Phaser.AUTO,
-        width: 800,
-        height: 600,
-        scene : {
-            preload : preload,
-            create: create,
-            update: update
-        }
-    }
-
-    var game = new Phaser.Game(game_engine_config);
-
-    function preload() {
-        console.log(config);
-        var team = shuffle(config.team);
-        var playerImagePaths = [];
-        team.forEach(function(player) {
-            playerImagePaths.push(config.resources + '/' + player.toLowerCase() + '.png');
+var app = {
+    setConfig: function (config) {
+        this.cfg = config;
+    },
+    preload: function () {
+        var game = this;
+        var team = shuffle(app.cfg.team);
+        app.cfg.team = team;
+        team.forEach(function (player) {
+            var imgPath = app.cfg.resources + '/' + player.toLowerCase() + '.png';
+            game.load.image(player, imgPath);
         });
-        // TODO continue
-        // var playerImages = [];
+        console.log(game);
+
+    },
+    create: function () {
+        var game = this;
+        var team = app.cfg.team;
+        game.add.image(400, 10, team[0]).setOrigin(0,0);
+    },
+    update: function () {
     }
+}
 
-    function create() {
-
-    }
-
-    function update() {
-
+var game_engine_config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: app.preload,
+        create: app.create,
+        update: app.update
     }
 }
