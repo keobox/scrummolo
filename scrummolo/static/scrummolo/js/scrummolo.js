d262791272578045e375923f5ae36be6c90ebd10
Cesare Placanica <ceplacan@cisco.com>
2019-04-18 10:32:02 +0200

    Game Logic

--- scrummolo/static/scrummolo/js/scrummolo.js
+++ scrummolo/static/scrummolo/js/scrummolo.js
@@ -30,7 +30,7 @@ function resize(width, height, maxWidth, maxHeight) {
 }
 
 function resizeImage(image) {
-    var dimensions = resize(image.displayWidth, image.displayHeight, app.width / 2, app.height / 2);
+    var dimensions = resize(image.width, image.height, app.width / 2, app.height / 2);
     image.displayWidth = dimensions[0];
     image.displayHeight = dimensions[1];
 }
@@ -43,6 +43,7 @@ var app = {
     yMargin: 10,
     playerIndex: 0,
     questionIndex: 0,
+    gameOver: false,
     setConfig: function (config) {
         this.cfg = config;
         this.questions = config.questions;
@@ -52,29 +53,49 @@ var app = {
     },
     getQuestion: function () {
         return this.questions[this.questionIndex];
+    },
+    step: function () {
+        if (this.questionIndex < this.questions.length - 1) {
+            this.questionIndex++;
+        } else {
+            if (this.playerIndex === this.team.length -1) {
+                this.gameOver = true;
+            } else {
+                this.questionIndex = 0;
+                this.playerIndex++;
+            }
+        }
     }
 }
 
 var gameLoop = {
     preload: function () {
-        var game = this;
+        app.game = this;
         var team = shuffle(app.cfg.team);
         app.team = team;
         team.forEach(function (player) {
             var imgPath = app.cfg.resources + '/' + player.toLowerCase() + '.png';
-            game.load.image(player, imgPath);
+            app.game.load.image(player, imgPath);
         });
     },
     create: function () {
-        var game = this;
-        var playerImage = game.add.image(app.width / 2, app.yMargin, app.team[0]).setOrigin(0, 0);
+        var playerImage = app.game.add.image(app.width / 2, app.yMargin, app.team[0]).setOrigin(0, 0);
         resizeImage(playerImage);
         app.playerImage = playerImage;
-        app.playerText = game.add.text(100, 100, app.getPlayer() + ', is your turn!', { font: '20px Arial', fill: '#fff' });
-        app.questionsText = game.add.text(100, 500, app.getQuestion(), { font: '40px Arial', fill: '#fff' });
-        app.stepKey = game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        app.playerText = app.game.add.text(100, 100, app.getPlayer() + ', is your turn!', { font: '20px Arial', fill: '#fff' });
+        app.questionsText = app.game.add.text(100, 500, app.getQuestion(), { font: '40px Arial', fill: '#fff' });
+        app.spaceKey = app.game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     },
     update: function () {
+        if (Phaser.Input.Keyboard.JustDown(app.spaceKey)) {
+            if (!app.gameOver) {
+                app.step();
+                app.questionsText.setText(app.getQuestion());
+                app.playerText.setText(app.getPlayer() + ', is your turn!');
+                app.playerImage.setTexture(app.getPlayer());
+                resizeImage(app.playerImage);
+            }
+        }
     }
 
 }
