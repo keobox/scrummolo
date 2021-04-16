8259c0a49c72671c416f548af8adf1f90d144b8e
Cesare Placanica <cesare.placanica@vx.se>
2021-04-16 14:46:05 +0200

    Flask only for API now

--- scrummolo/scrummolo_web.py
+++ scrummolo/scrummolo_web.py
@@ -1,41 +1,12 @@
 """API for scrummolo web."""
 
 import flask
+import json
 
 app = flask.Flask(__name__)
 
-assets_path = "cards"
-
-teams = [
-    {
-        "id": 1,
-        "team": ["Paolo", "Christian", "Marco"],
-        "duration": 30,
-        "assets": assets_path,
-        "atlas": "cards",
-        "gameOverImage": assets_path + "/" + "gameover.png",
-        "gameOverSound": assets_path + "/" + "gameover.wav",
-        "gameOverText": "That's all Folks, Thank You!",
-        "questions": [
-            "On what I worked yesterday?",
-            "On what I will work today?",
-            "Do I have any blocker?",
-        ],
-    }
-]
-
-
-@app.route("/")
-@app.route("/index")
-def index():
-    """Main page."""
-    return flask.send_from_directory("static/scrummolo", "index.html")
-
-
-@app.route("/<path:path>")
-def assets(path):
-    """Images and sounds."""
-    return flask.send_from_directory("static/scrummolo/assets", path)
+with open("scrummolo/static/scrummolo/js/config.json") as cfg:
+    teams = json.load(cfg)
 
 
 @app.route("/teams", methods=["GET"])
@@ -49,7 +20,7 @@ def get_team(team_id):
     """Returns a team resource given an id:int."""
     # assuming the list is ordered by id.
     try:
-        return {"team": teams[team_id - 1]}
+        return {"team": teams["teams"][team_id - 1]}
     except IndexError:
         flask.abort(404)
 
