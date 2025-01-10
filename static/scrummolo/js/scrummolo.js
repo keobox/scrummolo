58c6a43ce58c370ac192008949fd0cc34c4ac8c9
Cesare Placanica <1519356+keobox@users.noreply.github.com>
2025-01-10 21:46:01 +0100

    Remove the 'groups' feature

--- static/scrummolo/js/scrummolo.js
+++ static/scrummolo/js/scrummolo.js
@@ -54,13 +54,7 @@ var app = {
         this.duration = team.duration;
         this.questions = team.questions;
         this.timerSeconds = team.duration * 60;
-        if (team.players.length > 0) {
-            this.players = shuffle(team.players);
-        } else {
-            var groups = team.groups;
-            groups.map(function(group) {shuffle(group)});
-            this.players = groups.flat();
-        }
+        this.players = shuffle(team.players);
     },
     getPlayer: function () {
         return this.players[this.playerIndex];
